46883e4bde4bd5d6301667719066ac9d6e1f8c23
Abhishek Sharma <abhishek.sharma@74542@gmail.com>
2023-09-29 11:05:13 +0530

    added all apis

--- api_folder/service.py
+++ api_folder/service.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from src.controllers.dataframe_generator import OpenAIController
 
 
@@ -8,17 +8,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    df = openAIController.print_df()
+    df = openAIController.create_embeddings()
     return df
 
-@app.route('/create_cluster')
-def perform_clustering():
+@app.route('/clustering')
+def clustering():
     df = openAIController.perform_clustering()
     return df
 
 @app.route('/q_and_a')
 def perform_q_and_a():
-    df = openAIController.perform_qanda()
+    query = request.args.get('query')
+    df = openAIController.perform_qanda(query)
+    return df
+
+@app.route('/sample_json')
+def perform_json():
+    df = openAIController.perform_json()
+    return df
+
+@app.route('/semantic_search')
+def semantic_search():
+    search_term = request.args.get('search_term')
+    df = openAIController.semantic_search(search_term)
     return df
 
 if __name__ == '__main__':
--- api_folder/src/controllers/dataframe_generator.py
+++ api_folder/src/controllers/dataframe_generator.py
@@ -1,5 +1,7 @@
 import openai
+import json
 import pandas as pd
+from flask import jsonify
 import numpy as np
 from openai.embeddings_utils import get_embedding, cosine_similarity
 from sklearn.cluster import KMeans
@@ -9,6 +11,7 @@ from langchain.vectorstores.faiss import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain import OpenAI, VectorDBQA
 from src.controllers.longtext import state_of_the_union
+from src.controllers.sample import sample_json
 
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -17,54 +20,25 @@ texts = text_splitter.split_text(state_of_the_union)
 embeddings = OpenAIEmbeddings()
 vectorstore = FAISS.from_texts(texts, embeddings)
 
-openai_api_key = "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"
-os.environ["OPENAI_API_KEY"] = openai_api_key
-
-#@title State of Union Text
-
-#@title Environment Vars
-openai_api_key = "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"
-
 class OpenAIController:
     def __init__(self):
-        openai.api_key = "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"
+        openai.api_key = "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"
 
-    def print_df(self):
+
+    def create_embeddings(self):
         df = pd.read_csv('words.csv')
         print(df)
-        embeddings_sample = get_embedding("the fox crossed the road", engine='text-embedding-ada-002')
-        print(embeddings_sample)
         df['embedding'] = df['text'].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
         df.to_csv('word_embeddings.csv')
         df = pd.read_csv('word_embeddings.csv')
-        df['embedding'] = df['embedding'].apply(eval).apply(np.array)
-        print(df)
-        search_term = "coffee"
-        search_term_vector = get_embedding(search_term, engine="text-embedding-ada-002")
-        df["similarities"] = df['embedding'].apply(lambda x: cosine_similarity(x, search_term_vector))
-        df.sort_values("similarities", ascending=False).head(20)
-        print(df)
-        return "printed df"
+        selected_columns = ["Term", "text"]
+        selected_df = df[selected_columns]
+        json_data = selected_df.to_json(orient='records')
+        return json_data
     
     def perform_clustering(self):
         df = pd.read_csv('word_embeddings.csv')
         corpus = df['text'].to_numpy().tolist()
-        corpus2 = ['A man is eating food.',
-          'A man is eating a piece of bread.',
-          'Horse is eating grass.',
-          'A man is eating pasta.',
-          'A Woman is eating Biryani.',
-          'The girl is carrying a baby.',
-          'The baby is carried by the woman',
-          'A man is riding a horse.',
-          'A man is riding a white horse on an enclosed ground.',
-          'A monkey is playing drums.',
-          'Someone in a gorilla costume is playing a set of drums.',
-          'A cheetah is running behind its prey.',
-          'A cheetah chases prey on across a field.',
-          'The cheetah is chasing a man who is riding the horse.',
-          'man and women with their baby are watching cheetah in zoo'
-          ]
         print(corpus)
         response = openai.Embedding.create(
             input=corpus,
@@ -78,25 +52,45 @@ class OpenAIController:
         cluster_assignment = clustering_model.labels_
         clustered_sentences = {}
         for sentence_id, cluster_id in enumerate(cluster_assignment):
-            if cluster_id not in clustered_sentences:
-                clustered_sentences[cluster_id] = []
+            if str(cluster_id) not in clustered_sentences:
+                clustered_sentences[str(cluster_id)] = []
 
-            clustered_sentences[cluster_id].append(corpus[sentence_id])
-        print(corpus)
-        print(corpus2)
+            clustered_sentences[str(cluster_id)].append(corpus[sentence_id])
+        print("Clustered sentences  --------------------------------")
         print(clustered_sentences)
         
-        return "Printed array for clustering"
+        return {"clustered_sentences": clustered_sentences}, 200
     
-    def perform_qanda(self):
+    def perform_qanda(self, query):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.split_text(state_of_the_union)
-
-        embeddings = OpenAIEmbeddings(openai_api_key = openai_api_key)
+        api_key = "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"
+        embeddings = OpenAIEmbeddings(openai_api_key = api_key)
         vectorstore = FAISS.from_texts(texts, embeddings)
         print("Printing Vector store")
         print(vectorstore)
         qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=vectorstore)
-        query = "What did the president say about Ketanji Brown Jackson"
         query_result = qa.run(query)
         return query_result
+    
+    def perform_json(self):
+        df = pd.DataFrame(sample_json)
+
+        # Specify the file path where you want to save the CSV file
+        csv_file_path = 'words.csv'
+
+        # Save the DataFrame to a CSV file
+        df.to_csv(csv_file_path, index=False) 
+        return sample_json
+    
+    def semantic_search(self, search_term):
+        # Specify the file path where you want to save the CSV file
+        df = pd.read_csv('word_embeddings.csv')
+        df['embedding'] = df['embedding'].apply(eval).apply(np.array)
+        search_term_vector = get_embedding(search_term, engine="text-embedding-ada-002")
+        df["similarities"] = df['embedding'].apply(lambda x: cosine_similarity(x, search_term_vector))
+        df.sort_values("similarities", ascending=False, inplace=True)
+        json_data = json.loads(df.to_json(orient='records'))
+        for obj in json_data:
+            del obj["embedding"]
+        return json_data
